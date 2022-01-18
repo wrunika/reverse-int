ebbdec7f7aa7be58906ae6233e77aa3515617116
wrunika <2805nika@gmail.com>
2022-01-18 14:59:13 +0300

    fix: do task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
-}
+  let str = String(Math.abs(n));
+  let i = 0;
+  let result = '';
+
+  while (i < str.length) {
+    result = `${str[i]}${result}`;
+    i = i + 1;
+  }
+  return +result;
+};
